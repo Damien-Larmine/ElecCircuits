4c134423fdaaf8a908691b92c2ce0a5d3ba22e83
Deckao <Deckao@users.noreply.github.com>
2014-05-28 21:17:45 +0200

    Update CompXOR.java

--- CompXOR.java
+++ CompXOR.java
@@ -1,37 +1,15 @@
 public class CompXOR extends Component {
 
-	private boolean inA;
-	private boolean inB;
-
 	public CompXOR(String name) {
-		super(name, 2, 1);
-
-		this.inA = false;
-		this.inB = false;
-	}
-
-	public boolean getInA() {
-		return this.inA;
-	}
-
-	public boolean getInB() {
-		return this.inB;
+		super(name);
 	}
 
 	public boolean getOut() {
-		if (this.inA == this.inB) {
+		if (this.getInA() == this.getInB()) {
 			return false;
 		} else {
 			return true;
 		}
-	}
-
-	public void setInA(boolean valeur) {
-		this.inA = valeur;
-	}
-
-	public void setInB(boolean valeur) {
-		this.inB = valeur;
 	}
 
 }
