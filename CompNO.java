86443dba5b2ef560805129402106ebfac29e11b7
Deckao <Deckao@users.noreply.github.com>
2014-05-28 21:16:47 +0200

    Update CompNO.java

--- CompNO.java
+++ CompNO.java
@@ -1,23 +1,12 @@
 public class CompNO extends Component {
 
-	private boolean in;
 
 	public CompNO(String name) {
-		super(name, 1, 1);
-
-		this.in = false;
-	}
-
-	public boolean getIn() {
-		return this.in;
+		super(name);
 	}
 
 	public boolean getOut() {
-		return (!this.in);
-	}
-
-	public void setIn(boolean valeur) {
-		this.in = valeur;
+		return (!this.getInA());
 	}
 
 }
